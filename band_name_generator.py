6dd02c843760d83f3522ee607c0c9094bad0ff9c
codenamejj <jmtagare@gmail.com>
2023-01-12 06:25:47 +0000

    first program

---
+++ band_name_generator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("Welcome to the Band Name Generator.")
+town_name = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+band_name = town_name + " " + pet
+print('{}'.format(band_name), end="")
+print()
