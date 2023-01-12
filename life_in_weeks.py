bb82b829e00bb173998e729b979baa624084bae0
codenamejj <jmtagare@gmail.com>
2023-01-12 14:01:51 +0000

    program calculating the amount of months, weeks and days you have left

---
+++ life_in_weeks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# íº¨ Don't change the code below í±‡
+age = input("What is your current age?\n")
+life_expectancy = input("How many years in total do you expect to live?\n")
+# íº¨ Don't change the code above í±†
+
+#Write your code below this line í±‡
+years_left = int(life_expectancy) - int(age)
+days_left = years_left * 365
+weeks_left = years_left * 52
+months_left = years_left * 12
+
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
