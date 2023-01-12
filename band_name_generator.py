ed6f88cfe3951e94f7cc8fc33d18bdb5e1b1fe80
codenamejj <jmtagare@gmail.com>
2023-01-12 08:53:48 +0000

    updated

--- band_name_generator.py
+++ band_name_generator.py
@@ -4,5 +4,5 @@ town_name = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
 
 band_name = town_name + " " + pet
-print('{}'.format(band_name), end="")
+print('Your band name could be: {}'.format(band_name), end="")
 print()
